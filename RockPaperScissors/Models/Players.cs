0b07858bed404a0fc14dcfb0e70eeec389f5cd24
Keturah Howard <keturah.dev@gmail.com>
2020-03-04 13:40:09 -0800

    create functionality to pass test

--- RockPaperScissors/Models/Players.cs
+++ RockPaperScissors/Models/Players.cs
@@ -3,9 +3,11 @@ namespace RockPaperScissors.Models
   public class Player
   {
     public string Name{get;set;}
+    public string Weapon{get;set;}
     public Player(string name)
     {
       Name = name;
+      Weapon = "none";
     }
   }
 }
