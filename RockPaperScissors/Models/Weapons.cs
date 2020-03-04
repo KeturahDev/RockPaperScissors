93c244991510e1b49466b4dd92023e8492ea97a2
Keturah Howard <keturah.dev@gmail.com>
2020-03-04 14:35:24 -0800

    pass. create functionality of assainging weapon a specific type

--- RockPaperScissors/Models/Weapons.cs
+++ RockPaperScissors/Models/Weapons.cs
@@ -1,3 +1,5 @@
+using System;
+
 namespace RockPaperScissors.Models
 {
   public class Weapon
@@ -12,7 +14,23 @@ namespace RockPaperScissors.Models
     }
     public void AssaignType(string rquestedType)
     {
-      Type = "fail test";
+      rquestedType.ToLower();
+      if (rquestedType == "rock" || rquestedType == "r")
+      {
+        Type = options[0];
+      }
+      else if (rquestedType == "paper" || rquestedType == "p")
+      {
+        Type = options[1];
+      }
+      else if (rquestedType == "scissors" || rquestedType == "s")
+      {
+        Type = options[2];
+      }
+      else
+      {
+        //potentially use try catch block here, with if statments in try, and else acting as catch, and asking user to input correct inputs
+      }
     }
   }
 }
