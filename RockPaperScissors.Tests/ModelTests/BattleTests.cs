caecf77682538f46aa845eea7bfde01f4978dea7
Keturah Howard <keturah.dev@gmail.com>
2020-03-04 15:13:39 -0800

    create new class, fail first test for Battle spec

---
+++ RockPaperScissors.Tests/ModelTests/BattleTests.cs
@@ -0,0 +1,21 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using RockPaperScissors.Models;
+
+namespace RockPaperScissors.Tests 
+{
+  [TestClass]
+  public class BattleTests
+  {
+    [TestMethod]
+    public void CreateBattle_CreateBattle_TypeBattle()
+    {
+      Player player1 = new Player("Ven");
+      Player player2 = new Player("Keturah");
+      Battle newBattle = new Battle(player1, player2);
+
+      Battle result = newBattle;
+
+      Assert.AreEqual(typeof(Battle), result);
+    }
+  }
+}
